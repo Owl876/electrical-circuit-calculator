09ca268be3223952a1c379d3946fa25992491b23
Owl876 <dobretskovda@gmail.com>
2024-01-17 04:20:14 +0300

    fourth commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,6 +17,14 @@ MainWindow::MainWindow(QWidget *parent)
 
 }
 
+// Глобальные переменные
+// Список ячеек
+int cell1 = 0, cell2 = 0, cell3=0, cell4=0, cell5=0, cell6=0;
+// Значения токов
+double voltage = 0;
+
+
+
 MainWindow::~MainWindow()
 {
     delete ui;
@@ -32,6 +40,7 @@ void MainWindow::on_istochnik1_clicked()
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/istochnik.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button1_1->setIcon(ico);
+        cell1=1;
 
     }
     if(ui->shema1_button2->isChecked())
@@ -39,19 +48,26 @@ void MainWindow::on_istochnik1_clicked()
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/istochnik.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button2_1->setIcon(ico);
+        cell2=1;
     }
     if(ui->shema1_button3->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/istochnik.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button3_1->setIcon(ico);
+        cell3=1;
     }
     if(ui->shema1_button4->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/istochnik.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button4_1->setIcon(ico);
+        cell4=1;
     }
+    qDebug() << "Cell1: " << cell1;
+    qDebug() << "Cell2: " << cell2;
+    qDebug() << "Cell3: " << cell3;
+    qDebug() << "Cell4: " << cell4;
 }
 
 void MainWindow::on_istochnik2_clicked()
@@ -61,25 +77,33 @@ void MainWindow::on_istochnik2_clicked()
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/istochnik2.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button1_1->setIcon(ico);
+        cell1=2;
     }
     if(ui->shema1_button2->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/istochnik2.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button2_1->setIcon(ico);
+        cell2=2;
     }
     if(ui->shema1_button3->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/istochnik2.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button3_1->setIcon(ico);
+        cell3=2;
     }
     if(ui->shema1_button4->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/istochnik2.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button4_1->setIcon(ico);
+        cell4=2;
     }
+    qDebug() << "Cell1: " << cell1;
+    qDebug() << "Cell2: " << cell2;
+    qDebug() << "Cell3: " << cell3;
+    qDebug() << "Cell4: " << cell4;
 }
 
 void MainWindow::on_potrebitel1_clicked()
@@ -89,24 +113,28 @@ void MainWindow::on_potrebitel1_clicked()
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/lamp.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button1_1->setIcon(ico);
+        cell1=3;
     }
     if(ui->shema1_button2->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/lamp.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button2_1->setIcon(ico);
+        cell2=3;
     }
     if(ui->shema1_button3->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/lamp.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button3_1->setIcon(ico);
+        cell3=3;
     }
     if(ui->shema1_button4->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/lamp.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button4_1->setIcon(ico);
+        cell4=3;
     }
 }
 
@@ -117,24 +145,28 @@ void MainWindow::on_potrebitel2_clicked()
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/svetodiod.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button1_1->setIcon(ico);
+        cell1=4;
     }
     if(ui->shema1_button2->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/svetodiod.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button2_1->setIcon(ico);
+        cell2=4;
     }
     if(ui->shema1_button3->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/svetodiod.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button3_1->setIcon(ico);
+        cell3=4;
     }
     if(ui->shema1_button4->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/svetodiod.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button4_1->setIcon(ico);
+        cell4=4;
     }
 }
 
@@ -145,24 +177,28 @@ void MainWindow::on_condensator1_clicked()
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/postoiannii kondensator.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button1_1->setIcon(ico);
+        cell1=5;
     }
     if(ui->shema1_button2->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/postoiannii kondensator.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button2_1->setIcon(ico);
+        cell2=5;
     }
     if(ui->shema1_button3->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/postoiannii kondensator.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button3_1->setIcon(ico);
+        cell3=5;
     }
     if(ui->shema1_button4->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/postoiannii kondensator.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button4_1->setIcon(ico);
+        cell4=5;
     }
 }
 
@@ -173,24 +209,28 @@ void MainWindow::on_condensator2_clicked()
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/poliarizovanii kondensator.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button1_1->setIcon(ico);
+        cell1=6;
     }
     if(ui->shema1_button2->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/poliarizovanii kondensator.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button2_1->setIcon(ico);
+        cell2=6;
     }
     if(ui->shema1_button3->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/poliarizovanii kondensator.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button3_1->setIcon(ico);
+        cell3=6;
     }
     if(ui->shema1_button4->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/poliarizovanii kondensator.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button4_1->setIcon(ico);
+        cell4=6;
     }
 }
 
@@ -202,6 +242,7 @@ void MainWindow::on_rezistor_constant_clicked()
         ico.addPixmap(QPixmap(":/img/rezistor1.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button1_1->setIcon(ico);
         ui->label->setText("Сопротивление резистора");
+        cell1=7;
     }
     if(ui->shema1_button2->isChecked())
     {
@@ -209,6 +250,7 @@ void MainWindow::on_rezistor_constant_clicked()
         ico.addPixmap(QPixmap(":/img/rezistor1.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button2_1->setIcon(ico);
         ui->label->setText("Сопротивление резистора");
+        cell2=7;
     }
     if(ui->shema1_button3->isChecked())
     {
@@ -216,6 +258,7 @@ void MainWindow::on_rezistor_constant_clicked()
         ico.addPixmap(QPixmap(":/img/rezistor1.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button3_1->setIcon(ico);
         ui->label->setText("Сопротивление резистора");
+        cell3=7;
     }
     if(ui->shema1_button4->isChecked())
     {
@@ -223,6 +266,7 @@ void MainWindow::on_rezistor_constant_clicked()
         ico.addPixmap(QPixmap(":/img/rezistor1.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button4_1->setIcon(ico);
         ui->label->setText("Сопротивление резистора");
+        cell4=7;
     }
 
 }
@@ -234,24 +278,28 @@ void MainWindow::on_rezistor_variable_clicked()
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/rezistor2.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button1_1->setIcon(ico);
+        cell1=8;
     }
     if(ui->shema1_button2->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/rezistor2.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button2_1->setIcon(ico);
+        cell2=8;
     }
     if(ui->shema1_button3->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/rezistor2.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button3_1->setIcon(ico);
+        cell3=8;
     }
     if(ui->shema1_button4->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/rezistor2.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button4_1->setIcon(ico);
+        cell4=8;
     }
 }
 
@@ -262,24 +310,28 @@ void MainWindow::on_provodnik_clicked()
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/provodnik.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button1_1->setIcon(ico);
+        cell1=9;
     }
     if(ui->shema1_button2->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/provodnik.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button2_1->setIcon(ico);
+        cell2=9;
     }
     if(ui->shema1_button3->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/provodnik.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button3_1->setIcon(ico);
+        cell3=9;
     }
     if(ui->shema1_button4->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/provodnik.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button4_1->setIcon(ico);
+        cell4=9;
     }
 }
 
@@ -290,24 +342,28 @@ void MainWindow::on_key_clicked()
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/kluch.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button1_1->setIcon(ico);
+        cell1=10;
     }
     if(ui->shema1_button2->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/kluch.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button2_1->setIcon(ico);
+        cell2=10;
     }
     if(ui->shema1_button3->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/kluch.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button3_1->setIcon(ico);
+        cell3=10;
     }
     if(ui->shema1_button4->isChecked())
     {
         QIcon ico;
         ico.addPixmap(QPixmap(":/img/kluch.png"),QIcon::Normal,QIcon::On);
         ui->shema1_button4_1->setIcon(ico);
+        cell4=10;
     }
 }
 
@@ -331,6 +387,8 @@ void MainWindow::on_shema0_calculate_clicked()
 void MainWindow::slotForm(QString a)
 {
     ui->label->setText(a);
+    voltage = a.toDouble();
+    qDebug() << "voltage: " << voltage;
 }
 
 
